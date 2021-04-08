1621ff4f36cdeaa808bebe6ffbcf6d31bbb479ee
bjernie <rivall.bjorn@gmail.com>
2021-04-09 01:12:34 +0200

    stuff

--- src/components/NotePage/NotePage.js
+++ src/components/NotePage/NotePage.js
@@ -7,17 +7,27 @@ import BookmarkIcon from '@material-ui/icons/Bookmark';
 import testpb from '../../assets/testpb.jpg';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Snackbar from '@material-ui/core/Snackbar'
+import HashtagIcon from "../../custom/HashtagIcon";
 import {Link} from 'react-router-dom';
 import {apiUrl} from '../../index';
 import ArticlePanel from './ArticlePanel';
 import PDFPanel from './PDFPanel';
+import TagInput from "./TagInput";
 
 export default function NotePage(props) {
 
     const noteId = props.match.params.id;
     const [tabIndex, setTabIndex] = useState(0);
-    const [note, setNote] = useState({});
+    const [note, setNote] = useState({
+        title: 'Tv-seriers plads i det kulturelle landskab\n',
+        description: 'Opgavebeskrivelse: Skriv en debatterende artikel, hvor du med udgangspunkt i konkrete eksempler undersøger og diskuterer tv-seriens plads i det kulturelle landskab, og hvor du forsøger at overbevise din læser om dine egne synspunkter på tv-seriens potentialer og udfordringer som genre. Meh den blev vel ok?',
+        views: 23,
+        tags: ['dansk', 'engelsk'],
+        file: '6ukGe13m',
+        iso_date: '2020-04-12',
+        user: {username: 'Bjørn Rivall Andersen', currentUser: true},
+        saved: true
+    });
 
     useEffect(() => {
         async function getNote() {
@@ -26,9 +36,18 @@ export default function NotePage(props) {
             const noteJSON = await resp.json();
             setNote(noteJSON);
         }
-        getNote();
+        //getNote();
     }, []);
 
+
+    function convertDate() {
+        if (note.iso_date) {
+            const date = new Date(note.iso_date);
+            const options = {year: 'numeric', month: 'short', day: 'numeric'}
+            return date.toLocaleDateString(undefined, options);
+        }
+
+    }
 
     function save() {
         setNote({
@@ -59,13 +78,29 @@ export default function NotePage(props) {
                 <h3 className="text-xl text-gray-500 my-5">
                     {note.description}
                 </h3>
+                <div className="my-8">
+                    {note.user.currentUser ? (
+                        <TagInput inputTags={note.tags}/>
+                    ) : (
+                        <div className="flex flex-row">
+                            {note.tags.map(tag => {
+                                return (
+                                    <div className="flex flex-row items-center mr-2" key={tag}>
+                                        <HashtagIcon size={20} />
+                                        {tag}
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    )}
+                </div>
                 <div className="flex flex-1 flex-row items-center">
                     <img src={testpb} alt="profile" className="rounded-full w-7 h-7 mr-3" />
                     <div className="overflow-ellipsis overflow-hidden whitespace-nowrap">
                         <Link to="/">{note.user?.username}</Link>
                     </div>
                     <div className="ml-2 text-gray-500 md:text-base text-sm">
-                        {note.created}
+                        {convertDate()}
                         <span className="mx-2">·</span>
                         {note.views} visninger
                     </div>
